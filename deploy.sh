edf0cda42f227462d5b64d3fdf21e37df2e453c9
akshat103 <akshattamrakar103@gmail.com>
2024-05-14 21:40:28 +0530

    deploy

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Change directory to client
+cd client || exit
+
+# Install dependencies
+npm install
+
+# Build the React app
+npm run build
+
+# Return to the root folder
+cd ..
+
+# Install server dependencies
+npm install
+
+# Run the server
+node server.js
